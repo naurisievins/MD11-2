e17dd3720ed1e4a590531e4fd4d9e6a8d50ba401
naurisievins <nauris.ievins@gmail.com>
2023-01-25 20:24:56 +0200

    All functionality added

--- src/scripts.ts
+++ src/scripts.ts
@@ -2,29 +2,93 @@ import axios from 'axios';
 
 const _ = require('lodash');
 
-/* Sort by name
-http://localhost:3004/countries?_sort=name&_order=desc (asc)
-*/
+type Country = {
+  name: string,
+  code: string,
+  capital: string,
+  region: string,
+  currency: {
+    code: string,
+    name: string,
+    symbol: string
+  },
+  language: {
+    code: string,
+    name: string
+  },
+  flag: string,
+  dialling_code: string,
+  isoCode: string
+}
 
-/* Get 20 records (In the Link header you'll get first, prev, next and last links.)
-http://localhost:3004/countries?_page=1&_limit=20
-*/
-
-/* Select country by name
-http://localhost:3004/countries?name=Latvia
-*/
-
-/* Select by country language
-http://localhost:3004/countries?language.name=Latvian
-*/
 const url = new URL('http://localhost:3004/countries?_page=1&_limit=20');
 const tableBody = document.querySelector('.table__body');
 
+/* ############################# Pagination ############################# */
+
+const firstPageBtn:HTMLSpanElement = document.querySelector('.js-first-page');
+const prevPageBtn:HTMLSpanElement = document.querySelector('.js-prev-page');
+const nextPageBtn:HTMLSpanElement = document.querySelector('.js-next-page');
+const lastPageBtn:HTMLSpanElement = document.querySelector('.js-last-page');
+const pages:HTMLSpanElement = document.querySelector('.js-pages');
+let currentPage: string;
+let totalPages: string;
+let firstPage: string;
+let prevPage: string;
+let nextPage: string;
+let lastPage: string;
+
+const getCurrentPage = (link: string) => {
+  const trimmedLink: string = link.replace('http://localhost:3004/countries?_page=', '').slice(0, 3);
+  let result = '';
+  for (const char of trimmedLink) {
+    if (Number(char) < 10) {
+      result += char.toString();
+    }
+  }
+  return result;
+};
+
+const pagination = (link: string) => {
+  const links = link.split(' ');
+  if (links.length === 1) {
+    currentPage = '1';
+    totalPages = '1';
+    pages.innerHTML = `Page <b>${currentPage}</b> of <b> ${totalPages} </b>`;
+    firstPageBtn.classList.add('inactive');
+    prevPageBtn.classList.add('inactive');
+    nextPageBtn.classList.add('inactive');
+    lastPageBtn.classList.add('inactive');
+  } else {
+    if (links.length === 6) {
+      const nextOrPrev = link.split(' ')[3].slice(5, -2);
+      if (nextOrPrev === 'next') {
+        firstPage = links[0].slice(1, -2);
+        nextPage = links[2].slice(1, -2);
+        lastPage = links[4].slice(1, -2);
+        currentPage = (+getCurrentPage(nextPage) - 1).toString();
+      } else if (nextOrPrev === 'prev') {
+        firstPage = links[0].slice(1, -2);
+        prevPage = links[2].slice(1, -2);
+        lastPage = links[4].slice(1, -2);
+        currentPage = (+getCurrentPage(prevPage) + 1).toString();
+      }
+    } else if (links.length === 8) {
+      firstPage = links[0].slice(1, -2);
+      prevPage = links[2].slice(1, -2);
+      nextPage = links[4].slice(1, -2);
+      lastPage = links[6].slice(1, -2);
+      currentPage = (+getCurrentPage(prevPage) + 1).toString();
+    }
+    totalPages = getCurrentPage(lastPage);
+    pages.innerHTML = `Page <b>${currentPage}</b> of <b> ${totalPages} </b>`;
+  }
+};
+
 /* ############################# Insert country data in DOM ############################# */
+let id = 1;
 
-const insertCountryData = (data:any[]) => {
-  let id = 1;
-
+const insertCountryData = (data: Country[]) => {
   data.forEach((ele) => {
     const row = document.createElement('tr');
     row.classList.add('js-country-row');
@@ -49,41 +113,14 @@ const insertCountryData = (data:any[]) => {
   });
 };
 
-const pagination = (headers: any) => {
-  let firstPage: string;
-  let prevPage: string;
-  let nextPage: string;
-  let lastPage: string;
+/* ############################# reset to default ############################# */
 
-  const links = headers.link.split(' ');
-  if (links.length === 6) {
-    const nextOrPrev = headers.link.split(' ')[3].slice(5, -2);
-    if (nextOrPrev === 'next') {
-      firstPage = links[0].slice(1, -2);
-      nextPage = links[2].slice(1, -2);
-      lastPage = links[4].slice(1, -2);
-    } else if (nextOrPrev === 'prev') {
-      firstPage = links[0].slice(1, -2);
-      prevPage = links[2].slice(1, -2);
-      lastPage = links[4].slice(1, -2);
-    }
-  } else if (links.length === 8) {
-    firstPage = links[0].slice(1, -2);
-    prevPage = links[2].slice(1, -2);
-    nextPage = links[4].slice(1, -2);
-    lastPage = links[6].slice(1, -2);
-  }
-  console.log(`Previous: ${prevPage} First: ${firstPage} Next: ${nextPage} Last: ${lastPage}`)
-};
-
-/* ############################# Fetch country data ############################# */
-
-const FetchCountries = (apiUrl: URL) => {
-  axios.get(apiUrl.toString())
-    .then((response) => {
-      insertCountryData(response.data);
-      pagination(response.headers);
-    });
+const resetDefault = () => {
+  id = 1;
+  firstPageBtn.classList.add('inactive');
+  prevPageBtn.classList.add('inactive');
+  nextPageBtn.classList.remove('inactive');
+  lastPageBtn.classList.remove('inactive');
 };
 
 /* ############################# Delete all country rows from table ############################# */
@@ -93,6 +130,179 @@ const deleteCountries = () => {
     tableBody.removeChild(tableBody.childNodes[i]);
   }
 };
+
+/* ############################# Fetch country data ############################# */
+
+const fetchCountries = (apiUrl: URL | string) => {
+  axios.get(apiUrl.toString())
+    .then((response) => {
+      insertCountryData(response.data);
+      pagination(response.headers.link);
+    });
+};
+
+/* ############################# Pagination listeners ############################# */
+
+firstPageBtn.addEventListener('click', () => {
+  if (+totalPages > 1) {
+    nextPageBtn.classList.remove('inactive');
+    lastPageBtn.classList.remove('inactive');
+    if (!firstPageBtn.classList.contains('inactive')) {
+      id = 1;
+      deleteCountries();
+      fetchCountries(firstPage);
+      firstPageBtn.classList.add('inactive');
+      prevPageBtn.classList.add('inactive');
+    }
+  }
+});
+
+prevPageBtn.addEventListener('click', () => {
+  if (+totalPages > 1) {
+    nextPageBtn.classList.remove('inactive');
+    lastPageBtn.classList.remove('inactive');
+    if (currentPage === '2') {
+      prevPageBtn.classList.add('inactive');
+      firstPageBtn.classList.add('inactive');
+    }
+    if (currentPage !== '1') {
+      id = (Number(currentPage) - 1) * 20 - 19;
+      deleteCountries();
+      fetchCountries(prevPage);
+    }
+  }
+});
+
+nextPageBtn.addEventListener('click', () => {
+  if (+totalPages > 1) {
+    firstPageBtn.classList.remove('inactive');
+    prevPageBtn.classList.remove('inactive');
+    if (Number(currentPage) === (Number(totalPages) - 1)) {
+      nextPageBtn.classList.add('inactive');
+      lastPageBtn.classList.add('inactive');
+    }
+    if (Number(currentPage) !== (Number(totalPages))) {
+      id = (Number(currentPage) + 1) * 20 - 19;
+      deleteCountries();
+      fetchCountries(nextPage);
+    }
+  }
+});
+
+lastPageBtn.addEventListener('click', () => {
+  if (+totalPages > 1) {
+    firstPageBtn.classList.remove('inactive');
+    prevPageBtn.classList.remove('inactive');
+    if (!lastPageBtn.classList.contains('inactive')) {
+      nextPageBtn.classList.add('inactive');
+      lastPageBtn.classList.add('inactive');
+      id = Number(totalPages) * 20 - 19;
+      deleteCountries();
+      fetchCountries(lastPage);
+    }
+  }
+});
+
+/* ############################# Search function ############################# */
+
+let canSearch = true;
+const search = (searchFor: string, value: string) => {
+  canSearch = false;
+  let searchParams = '';
+  let searchURL:URL = url;
+  axios.get('http://localhost:3004/countries')
+    .then((response) => {
+      for (let i = 0; i < response.data.length; i += 1) {
+        const result: string = _.get(response.data[i], searchFor);
+        if (result.toLowerCase().includes(value.toLowerCase())) {
+          searchParams += `&${searchFor}=${result}`;
+        }
+        if (i === response.data.length - 1 && searchParams) {
+          canSearch = false;
+          searchURL = new URL(`${url.href}${searchParams.toString()}`);
+          // eslint-disable-next-line no-loop-func
+          const completeSearch = () => {
+            resetDefault();
+            deleteCountries();
+            fetchCountries(searchURL);
+            canSearch = true;
+            id = 1;
+          };
+          setTimeout(completeSearch, 500);
+        } else {
+          currentPage = '1';
+          totalPages = '1';
+          pages.innerHTML = `Page <b>${currentPage}</b> of <b> ${totalPages} </b>`;
+          deleteCountries();
+        }
+      }
+    });
+  canSearch = true;
+};
+
+const searchCountry:HTMLInputElement = document.querySelector('.js-country-search');
+const searchCapital:HTMLInputElement = document.querySelector('.js-capital-search');
+const searchCurrency:HTMLInputElement = document.querySelector('.js-currency-search');
+const searchLanguage:HTMLInputElement = document.querySelector('.js-language-search');
+
+const clearInputValues = () => {
+  searchCountry.value = '';
+  searchCapital.value = '';
+  searchCurrency.value = '';
+  searchLanguage.value = '';
+};
+
+searchCountry.addEventListener('keyup', () => {
+  searchCapital.value = '';
+  searchCurrency.value = '';
+  searchLanguage.value = '';
+  if (searchCountry.value.length > 2 && canSearch) {
+    search('name', searchCountry.value);
+  } else {
+    resetDefault();
+    deleteCountries();
+    fetchCountries(url);
+  }
+});
+
+searchCapital.addEventListener('keyup', () => {
+  searchCountry.value = '';
+  searchCurrency.value = '';
+  searchLanguage.value = '';
+  if (searchCapital.value.length > 2 && canSearch) {
+    search('capital', searchCapital.value);
+  } else {
+    resetDefault();
+    deleteCountries();
+    fetchCountries(url);
+  }
+});
+
+searchCurrency.addEventListener('keyup', () => {
+  searchCountry.value = '';
+  searchCapital.value = '';
+  searchLanguage.value = '';
+  if (searchCurrency.value.length > 2 && canSearch) {
+    search('currency.name', searchCurrency.value);
+  } else {
+    resetDefault();
+    deleteCountries();
+    fetchCountries(url);
+  }
+});
+
+searchLanguage.addEventListener('keyup', () => {
+  searchCountry.value = '';
+  searchCapital.value = '';
+  searchCurrency.value = '';
+  if (searchLanguage.value.length > 2 && canSearch) {
+    search('language.name', searchLanguage.value);
+  } else {
+    resetDefault();
+    deleteCountries();
+    fetchCountries(url);
+  }
+});
 
 /* ############################# Sort function, consts and listener ############################# */
 
@@ -109,7 +319,7 @@ const sort = (sortable: HTMLDivElement, sortBy: string) => {
   const searchParams = new URLSearchParams(sortParams);
   const sortURL = new URL(`${url.href}&${searchParams.toString()}`);
   deleteCountries();
-  FetchCountries(sortURL);
+  fetchCountries(sortURL);
 };
 
 const sortByName:HTMLDivElement = document.querySelector('.js-sort-name');
@@ -118,94 +328,45 @@ const sortByCurrency:HTMLDivElement = document.querySelector('.js-sort-currency'
 const sortByLanguage:HTMLDivElement = document.querySelector('.js-sort-language');
 
 sortByName.addEventListener('click', () => {
+  resetDefault();
+  clearInputValues();
   sort(sortByName, 'name');
 });
 
 sortByCapital.addEventListener('click', () => {
-  sort(sortByName, 'capital');
+  resetDefault();
+  clearInputValues();
+  sort(sortByCapital, 'capital');
 });
 
 sortByCurrency.addEventListener('click', () => {
-  sort(sortByName, 'currency.name');
+  resetDefault();
+  clearInputValues();
+  sort(sortByCurrency, 'currency.name');
 });
 
 sortByLanguage.addEventListener('click', () => {
-  sort(sortByName, 'language.name');
+  resetDefault();
+  clearInputValues();
+  sort(sortByLanguage, 'language.name');
 });
 
-/* ############################# Search function ############################# */
+/* ############################# On page load ############################# */
 
-let canSearch = true;
-const search = (searchFor: string, value: string) => {
-  canSearch = false;
-  let searchParams = '';
-  let searchURL:URL = url;
-  axios.get('http://localhost:3004/countries')
-    .then((response) => {
-      for (let i = 0; i < response.data.length; i += 1) {
-        const result: string = _.get(response.data[i], searchFor);
-        if (result.toLowerCase().includes(value.toLowerCase())) {
-          searchParams += `&${searchFor}=${result}`;
-          searchURL = new URL(`${url.href}${searchParams.toString()}`);
-        }
-        if (i === response.data.length - 1 && searchParams) {
-          canSearch = false;
-          // eslint-disable-next-line no-loop-func
-          const completeSearch = () => {
-            deleteCountries();
-            FetchCountries(searchURL);
-            canSearch = true;
-          };
-          setTimeout(completeSearch, 500);
-        } else {
-          deleteCountries();
-        }
-      }
-    });
-  canSearch = true;
-};
+fetchCountries(url);
 
-const searchCountry:HTMLInputElement = document.querySelector('.js-country-search');
-const searchCapital:HTMLInputElement = document.querySelector('.js-capital-search');
-const searchCurrency:HTMLInputElement = document.querySelector('.js-currency-search');
-const searchLanguage:HTMLInputElement = document.querySelector('.js-language-search');
+/* Sort by name
+http://localhost:3004/countries?_sort=name&_order=desc (asc)
+*/
 
-searchCountry.addEventListener('keyup', () => {
-  if (searchCountry.value.length > 2 && canSearch) {
-    search('name', searchCountry.value);
-  } else {
-    deleteCountries();
-    FetchCountries(url);
-  }
-});
+/* Get 20 records (In the Link header you'll get first, prev, next and last links.)
+http://localhost:3004/countries?_page=1&_limit=20
+*/
 
-searchCapital.addEventListener('keyup', () => {
-  if (searchCapital.value.length > 2 && canSearch) {
-    search('capital', searchCapital.value);
-  } else {
-    deleteCountries();
-    FetchCountries(url);
-  }
-});
+/* Select country by name
+http://localhost:3004/countries?name=Latvia
+*/
 
-searchCurrency.addEventListener('keyup', () => {
-  if (searchCurrency.value.length > 2 && canSearch) {
-    search('currency.name', searchCurrency.value);
-  } else {
-    deleteCountries();
-    FetchCountries(url);
-  }
-});
-
-searchLanguage.addEventListener('keyup', () => {
-  if (searchLanguage.value.length > 2 && canSearch) {
-    search('language.name', searchLanguage.value);
-  } else {
-    deleteCountries();
-    FetchCountries(url);
-  }
-});
-
-/* ############################# Fetch countries on page load ############################# */
-
-FetchCountries(url);
+/* Select by country language
+http://localhost:3004/countries?language.name=Latvian
+*/
